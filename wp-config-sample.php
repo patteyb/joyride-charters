cb5c98ca2d118563fb75939b33579842879f20fe
patteyb <patteybleecker@gmail.com>
2017-08-31 14:10:11 -0400

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'documentmanager');
+define('DB_NAME', 'heroku_048bed3bb41232b');
 
 /** MySQL database username */
 define('DB_USER', 'root');
@@ -46,14 +46,22 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
+/*define('AUTH_KEY',         'put your unique phrase here');
 define('SECURE_AUTH_KEY',  'put your unique phrase here');
 define('LOGGED_IN_KEY',    'put your unique phrase here');
 define('NONCE_KEY',        'put your unique phrase here');
 define('AUTH_SALT',        'put your unique phrase here');
 define('SECURE_AUTH_SALT', 'put your unique phrase here');
 define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');*/
+define('AUTH_KEY',         'yLvF3053g1?*oXf^|pc}ny@UE|[l6IZ(5lOOElZ=`w1Aa6wQ@<09sKh3C()_h{UD');
+define('SECURE_AUTH_KEY',  'P1>P%{se,8,F%A|]vWFm]IMI+!ZmWDP7eXTVe>+7n@gvT+#KH[F<jP5}cpjy-G.Q');
+define('LOGGED_IN_KEY',    'Ubng~gj7B<uWA4=5}8 upIE:oqd@L!Wb(AJ<cux;j/x>Mu:sr^>-pR+5lJ1}_Y8B');
+define('NONCE_KEY',        'ugFY=Sd9$yWKy=#9hCzk!+Yx7bvWMs{d2YHimqag<t(i!k5anR?I+Lx_n}Mc=G|B');
+define('AUTH_SALT',        'O%jiQZ%3e2:0VE6Avn2LNcB|.r|p}@YzQ.P|T;2.NdI3H*7ot+M?(]p1vF=))2dM');
+define('SECURE_AUTH_SALT', 'QgK-`^YQ-j5N_(O-W{]s|pC`iBd@cq}^,q!BpH8E;dIp6}C{;;h4gX3M|-k*-GYx');
+define('LOGGED_IN_SALT',   '*J#<t&fv!+]|E0=|>+w7|fbX[eMljgj8DxplzYe&|a=qt;IfU!=wEO?lBbDbwuS/');
+define('NONCE_SALT',       'CdYODlek<Uu|A@,,L#gDF`Kpq6Y7j@OvW?R4{v<4,-{b//jAM%u+yu-R)BM>rr*(');
 
 /**#@-*/
 
